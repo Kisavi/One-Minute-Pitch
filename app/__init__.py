5135ea28cdfeac955f483bfaa8c5d23ab017ac90
Kisavi <deniskisavi@gmail.com>
2022-05-08 18:40:30 +0300

    import and initialize bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask  # import the Flask class from flask module
 from config import config_options
+from flask_bootstrap import Bootstrap
 
 
 def create_app(config_name):  # a method to instantiate our App
@@ -14,5 +15,9 @@ def create_app(config_name):  # a method to instantiate our App
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/')
+
+    bootstrap = Bootstrap()  # Initializing Bootstrap class
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
     return app
