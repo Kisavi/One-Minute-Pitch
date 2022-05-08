9b8befeae6bbd541b094771aa4626f64ba8ff570
Kisavi <deniskisavi@gmail.com>
2022-05-08 03:19:33 +0300

    create configuration classes for our application

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask  # import the Flask class from flask module
+from config import config_options
 
 
-def create_app(): # a method to instantiate our App
+def create_app(config_name):  # a method to instantiate our App
     app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
     return app
