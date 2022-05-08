cf1c98bae1dd542c039767820e0f0d725ebb7a4e
Kisavi <deniskisavi@gmail.com>
2022-05-08 23:59:49 +0300

    add error and success messages on user sign up page

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from . import auth
-from flask import render_template, request
+from flask import render_template, request, flash
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -14,4 +14,21 @@ def logout():
 
 @auth.route('/sign-up', methods=["GET", "POST"])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(username) < 2:
+            flash('Pick a username that is greater than 2 characters.', category='error')
+        elif len(password1) < 7:
+            flash('Use a stronger password that is more than 7 characters long.', category='error')
+        elif password1 != password2:
+            flash('Passwords did not match!!!', category='error')
+        else:
+            flash('Account created successfully.', category='success')
+
     return render_template("auth/sign-up.html")
